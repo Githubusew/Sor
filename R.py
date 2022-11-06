8e1df195a1ef245239963f7492eedb50124a3407
Githubusew <117513595+Githubusew@users.noreply.github.com>
2022-11-06 13:34:41 +0530

    Update R.py

--- R.py
+++ R.py
@@ -1,22 +1,22 @@
-from smartapi import SmartConnect   
-token = "DCASFPWTRVAJML7KW2QBWJ2DAY"   
-import pyotp  
-pyotp.TOTP(token).now()   
- apikey = "KUiYK7su" 
+from smartapi import SmartConnect
+token = "DCASFPWTRVAJML7KW2QBWJ2DAY"
+import pyotp
+pyotp.TOTP(token).now()
+ apikey = "KUiYK7su"
  username = "a356453"
  pwd = "Home~641"
  token = "DCASFPWTRVAJML7KW2QBWJ2DAY"
- obj=SmartConnect(api_key=apikey) 
- data = obj.generateSession(username,pwd,pyotp.TOTP(token).now()) 
- print(data) 
- refreshToken= data['data']['refreshToken'] 
- res = obj.getProfile(refreshToken) 
- print(res)   
- from smartapi import SmartConnect 
- from datetime import datetime 
- import credentials 
- import requests 
- import numpy as np   
+ obj=SmartConnect(api_key=apikey)
+ data = obj.generateSession(username,pwd,pyotp.TOTP(token).now())
+ print(data)
+ refreshToken= data['data']['refreshToken']
+ res = obj.getProfile(refreshToken)
+ print(res)
+ from smartapi import SmartConnect
+ from datetime import datetime
+ import credentials
+ import requests
+ import numpy as np
 try:
     orderparams = {
         "variety": "NORMAL",
