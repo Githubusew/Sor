0f78d78141a6d15d01e1bd09c3d7dbd17268d335
Githubusew <117513595+Githubusew@users.noreply.github.com>
2022-11-06 13:33:08 +0530

    Update R.py

--- R.py
+++ R.py
@@ -1,11 +1,11 @@
 from smartapi import SmartConnect   
-token = 'DCASFPWTRVAJML7KW2QBWJ2DAY'   
+token = "DCASFPWTRVAJML7KW2QBWJ2DAY"   
 import pyotp  
 pyotp.TOTP(token).now()   
- apikey = 'KUiYK7su' 
- username = 'a356453' 
- pwd = 'Home~641' 
- token = 'DCASFPWTRVAJML7KW2QBWJ2DAY'   
+ apikey = "KUiYK7su" 
+ username = "a356453"
+ pwd = "Home~641"
+ token = "DCASFPWTRVAJML7KW2QBWJ2DAY"
  obj=SmartConnect(api_key=apikey) 
  data = obj.generateSession(username,pwd,pyotp.TOTP(token).now()) 
  print(data) 
