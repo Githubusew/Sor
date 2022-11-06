76e767fa5910277e4de83d09ba7828605c4205a5
Githubusew <117513595+Githubusew@users.noreply.github.com>
2022-11-06 14:09:37 +0530

    Update R.py

--- R.py
+++ R.py
@@ -1,17 +1,31 @@
-from smartapi import SmartConnect
-import pyotp
-pyotp.TOTP(token).now()
- obj=SmartConnect(api_key=apikey)
- data = obj.generateSession(username,pwd,pyotp.TOTP(token).now())
- print(data)
- refreshToken= data['data']['refreshToken']
- res = obj.getProfile(refreshToken)
- print(res)
- from smartapi import SmartConnect
- from datetime import datetime
- import credentials
- import requests
- import numpy as np
+from smartapi import SmartConnect 
+ import pyotp 
+  
+ token = 'DCASFPWTRVAJML7KW2QBWJ2DAY' 
+  
+ import pyotp 
+  
+ pyotp.TOTP(token).now() 
+  
+ apikey = 'KUiYK7su' 
+ username = 'a356453' 
+ pwd = 'Home~641' 
+ token = 'DCASFPWTRVAJML7KW2QBWJ2DAY' 
+  
+ obj=SmartConnect(api_key=apikey) 
+ data = obj.generateSession(username,pwd,pyotp.TOTP(token).now()) 
+ print(data) 
+ refreshToken= data['data']['refreshToken'] 
+ res = obj.getProfile(refreshToken) 
+ print(res) 
+  
+ from smartapi import SmartConnect 
+ from datetime import datetime 
+ import credentials 
+ import requests 
+ import numpy as np 
+  
+  
 try:
     orderparams = {
         "variety": "NORMAL",
@@ -30,4 +44,4 @@ try:
     orderId=obj.placeOrder(orderparams)
     print("The order id is: {}".format(orderId))
 except Exception as e:
-    print("Order placement failed:)
+    print("Order placement failed:
