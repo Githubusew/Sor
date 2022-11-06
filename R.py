d2ca68f7b80852765b033e8c7eab09e0ce79f53c
Githubusew <117513595+Githubusew@users.noreply.github.com>
2022-11-06 14:15:32 +0530

    Update R.py

--- R.py
+++ R.py
@@ -1,15 +1,15 @@
-from smartapi import SmartConnect 
- import pyotp 
-  token = ' DCASFPWTRVAJML7KW2QBWJ2DAY '  
- import pyotp   
- pyotp.TOTP(token).now()   
- apikey = ' KUiYK7su ' 
- username = ' a356453 ' 
- pwd = ' Home~641 ' 
- token = ' DCASFPWTRVAJML7KW2QBWJ2DAY '   
- obj=SmartConnect(api_key=apikey) 
- data = obj.generateSession(username,pwd,pyotp.TOTP(token).now()) 
- print(data) 
+from smartapi import SmartConnect
+import pyotp
+token = ' DCASFPWTRVAJML7KW2QBWJ2DAY '
+import pyotp
+pyotp.TOTP(token).now()
+apikey = ' KUiYK7su '
+username = ' a356453 '
+pwd = ' Home~641 '
+token = ' DCASFPWTRVAJML7KW2QBWJ2DAY '
+obj=SmartConnect(api_key=apikey)
+data = obj.generateSession(username,pwd,pyotp.TOTP(token).now())
+print(data) 
  refreshToken= data['data']['refreshToken'] 
  res = obj.getProfile(refreshToken) 
  print(res)   
