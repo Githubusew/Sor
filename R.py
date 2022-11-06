c4e3a83cba3c8cbef9a98dc937f1c72dab7ded94
Githubusew <117513595+Githubusew@users.noreply.github.com>
2022-11-06 14:30:19 +0530

    Update R.py

--- R.py
+++ R.py
@@ -22,5 +22,3 @@ orderparams = {
 "price": "19500","squareoff": "0","stoploss": "0","quantity": "1" }
 orderId=obj.placeOrder(orderparams)
 print("The order id is: {}".format(orderId))
-except Exception as e:
-print("Order placement failed:
