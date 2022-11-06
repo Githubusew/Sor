0b93fd35b96d578cee9bd2411a4e97ee7b2ba378
Githubusew <117513595+Githubusew@users.noreply.github.com>
2022-11-06 14:27:31 +0530

    Update R.py

--- R.py
+++ R.py
@@ -9,4 +9,18 @@ pwd = ' Home~641 '
 token = ' DCASFPWTRVAJML7KW2QBWJ2DAY '
 obj=SmartConnect(api_key=apikey)
 data = obj.generateSession(username,pwd,pyotp.TOTP(token).now())
-print(data)
+refreshToken= data['data']['refreshToken']
+res = obj.getProfile(refreshToken)
+from smartapi import SmartConnect
+from datetime import datetime
+import credentials
+import requests
+import numpy as np   
+try:
+orderparams = {
+"variety": "NORMAL","tradingsymbol": "SBIN-EQ","symboltoken": "3045","transactiontype": "BUY","exchange": "NSE","ordertype": "LIMIT","producttype": "INTRADAY","duration": "DAY",
+"price": "19500","squareoff": "0","stoploss": "0","quantity": "1" }
+orderId=obj.placeOrder(orderparams)
+print("The order id is: {}".format(orderId))
+except Exception as e:
+print("Order placement failed:
